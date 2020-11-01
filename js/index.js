845556eccc0737727ef9adcec2966da685cac455
Bui Quoc Trieu <buiquoctrieu0301@gmail.com>
2020-11-01 12:10:15 +1100

    fix blog section

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
-  nav: false,
+  nav: true,
   responsive: {
     0: {
       items: 1,
     },
   },
+});
+
+$(".counter").countUp({
+  delay: 10,
+  time: 2000,
 });
